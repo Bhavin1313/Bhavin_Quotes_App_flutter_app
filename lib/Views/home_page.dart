9f7ae6f419627159edfe276cbb497cda1ac91f7d
bhavin bhalala <bhavin1313bhalala@gmail.com>
2023-08-04 16:55:35 +0530

    First Commit

---
+++ lib/Views/home_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:quotes_app/Components/Utils/list_quotes.dart';
+
+import '../Model/quotes_model.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<Quotes> QuotsData;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    QuotsData = QuoteList.map((e) => Quotes.fromMap(Data: e)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int counter = 1;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home Page"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: QuotsData.map(
+              (e) => Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        "${counter++}. ${e.quote}",
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "Autor :- ${e.author}",
+                          style:
+                              TextStyle(fontSize: 10, color: Colors.blueGrey),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                elevation: 2,
+              ),
+            ).toList(),
+          ),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'Views/home_page.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      routes: {
+        '/': (context) => HomePage(),
+      },
+    ),
+  );
+}
